5c9150c21561dc107f22d77a1d245f1fa18effa6
almaa <almaa@ADRIANA>
2023-03-17 21:53:58 +0200

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Writely;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
