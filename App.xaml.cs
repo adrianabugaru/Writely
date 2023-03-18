1093798242376514fcf4b031810f8ad8a7daaa44
addasoft <abugaru@gmail.com>
2023-03-19 00:12:50 +0200

    added the option that main page would disappear after first using

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,10 +2,25 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        if (Preferences.ContainsKey("IsFirstLaunch"))
+        {
+            // App has already launched before
+            MainPage = new NavigationPage(new HomePage());
+        }
+        else
+        {
+            // First launch
+            MainPage = new NavigationPage(new MainPage());
+
+            // Save the "IsFirstLaunch" flag
+            Preferences.Set("IsFirstLaunch", false);
+        }
+    }
+
+   
 }
+
